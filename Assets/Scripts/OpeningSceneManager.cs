ad54e1766fa0aa5e1f269036f762c54ae4dd17b1
ZhamesK <jaehyug210@gmail.com>
2024-05-13 15:54:56 +0900

    [Feat] Opening Scene
    이름 입력
    캐릭터 선택
    메인 씬 이동

---
+++ Assets/Scripts/OpeningSceneManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OpeningSceneManager : MonoBehaviour
+{
+    private InputField InputName;
+    
+    public Button JoinButton;       // "Join" 버튼
+    public Button SelectCharButton; // 캐릭터 선택 버튼
+    public Button PenguinButton;    // 캐릭터 펭귄 선택 버튼
+    public Button GreenButton;      // 캐릭터 2 선택 버튼
+
+    public Sprite SpritePenguin;
+    public Sprite SpriteGreen;
+
+    public SpriteRenderer characterRenderer;
+
+    public GameObject CharacterBox; // 캐릭터 창
+    public GameObject SelectBox;    // 캐릭터 선택 창
+    public GameObject NameBox;      // 이름 입력 박스
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JoinButton.onClick.AddListener(NextScene);
+        SelectCharButton.onClick.AddListener(ShowSelectMenu);
+        PenguinButton.onClick.AddListener(SelectPenguin);
+        GreenButton.onClick.AddListener(SelectGreen);
+    }
+
+    public string SaveName()
+    {
+        return InputName.text;
+    }
+
+    // 이름 입력 후 다음 씬 이동
+    public void NextScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    // 캐릭터 창 클릭 후 캐릭터 선택 창 활성화
+    public void ShowSelectMenu()
+    {
+        SelectBox.SetActive(true);
+        NameBox.SetActive(false);
+        CharacterBox.SetActive(false);
+    }
+
+    // 펭귄 캐릭터 선택 후 캐릭터 창에 펭귄 캐릭터 스프라이트 반영
+    public void SelectPenguin()
+    {
+        SelectBox.SetActive(false);
+        NameBox.SetActive(true);
+        CharacterBox.SetActive(true);
+        characterRenderer.sprite = SpritePenguin;
+    }
+
+    // ~ Green 캐릭터 스프라이트 반영
+    public void SelectGreen()
+    {
+        SelectBox.SetActive(false);
+        NameBox.SetActive(true);
+        CharacterBox.SetActive(true);
+        characterRenderer.sprite = SpriteGreen;
+    }
+}
