5a86f99435592dda82967bf44eac7069ab376135
ZhamesK <jaehyug210@gmail.com>
2024-05-14 11:23:52 +0900

    [Feat] Time 표시

--- Assets/Scripts/MainSceneManager.cs
+++ Assets/Scripts/MainSceneManager.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainSceneManager : MonoBehaviour
 {
     public Text NameTxt;
+    public TextMeshProUGUI TimeTxt;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +20,7 @@ public class MainSceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TimeTxt.text = GetCurrentTime();
     }
 
     public void LoadName()
@@ -29,5 +33,12 @@ public class MainSceneManager : MonoBehaviour
         {
             Debug.Log("No Name");
         }
+    }
+
+
+    // 현재 시간
+    public string GetCurrentTime()
+    {
+        return DateTime.Now.ToString("HH:mm:ss");
     }
 }
